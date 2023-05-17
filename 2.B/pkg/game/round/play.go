500397da1ae5b9be6810d623bb863476a84f81ee
adrian <adrian.lin.1.0.0@gmail.com>
2023-05-17 11:57:17 +0800

    draft

--- 2.B/pkg/game/round/play.go
+++ 2.B/pkg/game/round/play.go
@@ -1,19 +1,17 @@
 package round
 
 import (
-	"big2/pkg/card"
 	"big2/pkg/card/patterns"
 	"big2/pkg/game"
 	"big2/pkg/notify/message"
-	"big2/pkg/player"
 	"fmt"
 )
+
+var DefaultPlay = NewPlay(patterns.CardPatternsFactory())
 
 func NewPlay(cardPatternsChain *patterns.CardPatternsChain) func(*game.BigTwo) {
 	return func(b *game.BigTwo) {
 
-		passCount := 0
-		passLimit := len(b.Players) - 1
 		playerCircular := NewPlayerCircular(b.Players)
 
 		for {
@@ -25,6 +23,8 @@ func NewPlay(cardPatternsChain *patterns.CardPatternsChain) func(*game.BigTwo) {
 			//TODO: handle no club3
 		}
 
+		passCount := 0
+		passLimit := len(b.Players) - 1
 		isFirstRound := true
 
 		for {
@@ -102,51 +102,4 @@ func NewPlay(cardPatternsChain *patterns.CardPatternsChain) func(*game.BigTwo) {
 		}
 
 	}
-}
-
-func hasClub3(cards []card.Card) bool {
-	return hasCard(cards, card.Card{Suit: card.Clubs, Rank: card.Three})
-}
-
-func hasCard(cards []card.Card, card card.Card) bool {
-	for _, c := range cards {
-		if c == card {
-			return true
-		}
-	}
-	return false
-}
-
-type playerCircular struct {
-	next   *playerCircular
-	Player *player.Player
-}
-
-func (p *playerCircular) Next() *playerCircular {
-	p = p.next
-	return p
-}
-
-func (p *playerCircular) GetPlayer() *player.Player {
-	return p.Player
-}
-
-func NewPlayerCircular(players []*player.Player) *playerCircular {
-	var head *playerCircular
-	var tail *playerCircular
-	for _, p := range players {
-		if head == nil {
-			head = &playerCircular{
-				Player: p,
-			}
-			tail = head
-			continue
-		}
-		tail.next = &playerCircular{
-			Player: p,
-		}
-		tail = tail.next
-	}
-	tail.next = head
-	return head
 }
